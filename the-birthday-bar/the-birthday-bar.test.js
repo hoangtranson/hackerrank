8a18faaebc8c4a32cdedd48c3e203d9e0b8eafa1
hoangtranson <son.hoang01@gmail.com>
2024-02-29 14:05:02 +0700

    refactor: 💡 add more test cases

--- the-birthday-bar/the-birthday-bar.test.js
+++ the-birthday-bar/the-birthday-bar.test.js
@@ -1,5 +1,15 @@
 const birthday = require('./the-birthday-bar');
 
-test('test case 0', () => {
-    expect(birthday([1, 2, 1, 3, 2], 3, 2)).toBe(2);
+describe('the-birthday-bar', () => {
+    test('test case 1', () => {
+        expect(birthday([1, 2, 1, 3, 2], 3, 2)).toBe(2);
+    });
+    
+    test('test case 2', () => {
+        expect(birthday([1, 1, 1, 1, 1, 1], 3, 2)).toBe(0);
+    });
+
+    test('test case 3', () => {
+        expect(birthday([4], 4, 1)).toBe(1);
+    });
 });
